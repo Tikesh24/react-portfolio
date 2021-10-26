337953d08f1a65659e660f74477d5b4e77a0a0f9
Tikesh Verma <tikeshverma2494@gmail.com>
2021-10-27 02:01:45 +0530

    Update index.js

--- portfolio/config/index.js
+++ portfolio/config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? process.env.PUBLIC_API_URL : 'react-portfolio-tikesh24.vercel.app'
+export const server = dev ? process.env.PUBLIC_API_URL : process.env.PUBLIC_API_URL
