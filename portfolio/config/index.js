afba893bbbf80a7754e7be5f674aab4acfcbce7b
Tikesh Verma <tikeshverma2494@gmail.com>
2021-10-31 01:53:35 +0530

    Update index.js

--- portfolio/config/index.js
+++ portfolio/config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'react-portfolio-tikesh24.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://react-portfolio-tikesh24.vercel.app'
