6cdff43f00586ca0193abf5c9dc8125220871af0
Tikesh24 <tikeshverma2494@gmail.com>
2021-12-28 16:44:28 +0530

    refactor bugs

--- portfolio/components/Work/WorkThumbnail.js
+++ portfolio/components/Work/WorkThumbnail.js
@@ -1,19 +1,21 @@
 import Image from 'next/image';
-import NO_IMAGE from '../../public/assests/noimage.png'
+//import NO_IMAGE from '../../public/assests/noimage.png'
 import {ExternalLinkIcon} from '@heroicons/react/outline'
 import {CalendarIcon} from '@heroicons/react/outline'
 
 const WorkThumbnail = ({work,date,thumbicon,url,discription}) =>{
+
+    const no_img = '../../assests/noimage.png'
     return (
 
-        <div className=" font-light focus:border-b-2">
+        <div className="pt-8 font-light focus:border-b-2">
             <div className="relative pt-4 grid grid-cols-1 gap-2">
                 <div className="rounded overflow-hidden shadow-lg bg-black">
-                    <div className ="px-6 py-4 grid grid-cols-3 md:grid-cols-5 border-2  hover:border-2 border-transparent hover:border-green-600 transition delay-100 ease-in-out">
-                        <div className="col-span-1 row-start-1">
-                            <Image width="350" height="250" src={thumbicon!=="NA" && thumbicon!== undefined ?`${thumbicon}`:NO_IMAGE} />
+                    <div className ="px-6 py-4 grid grid-cols-2 grid-rows-2 md:grid-cols-5 border-2  hover:border-2 border-transparent hover:border-green-600 transition delay-100 ease-in-out">
+                        <div className="mx-auto row-span-2 col-span-2 md:col-span-1 md:col-start-1 md:-mx-0">
+                            <img width="350" height="250" src={thumbicon!=="NA" && thumbicon!== undefined ?`../../${thumbicon}`:no_img} />
                         </div>
-                        <div className="col-span-4 col-start-2 pl-8 my-auto">
+                        <div className="mx-auto row-span-2 col-span-2 md:col-span-4 md:col-start-2 md:-mx-0 pl-8 my-auto">
                             <div className ="text-2xl font-semibold tracking-widest pb-2 flex">
                                 <h6 className="text-2xl pl-2 text-green-500">{work}</h6>
                             </div>
